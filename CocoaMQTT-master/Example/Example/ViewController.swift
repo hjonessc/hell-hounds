dc7eb5124016b523fb33a1dda08e092ee87c0f0d
Henry <henryjonessc@gmail.com>
2024-10-27 20:10:57 -0700

    Refactor, improvements, separate master hellhound, loading states

--- CocoaMQTT-master/Example/Example/ViewController.swift
+++ CocoaMQTT-master/Example/Example/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 import CocoaMQTT
 
+struct Hellhound {
+    var ledSwitch: UISwitch
+    var ledSlider: UISlider
+    var battery: BatteryView
+    var view: UIView
+    var isConnected: Bool = false
+}
 
 class ViewController: UIViewController {
-//    let defaultHost = "127.0.0.1"
     let defaultHost      = "broker.hivemq.com"
     let kTopicPrefix     = "HellHounds-MQTT"
     let kTopicSetState   = "set-state"
@@ -25,6 +31,12 @@ class ViewController: UIViewController {
     
     let kNumHellhounds = 4
 
+    enum ConnectionState {
+        case connected
+        case connecting
+        case disconnected
+    }
+
     var mqtt: CocoaMQTT?
     
     @IBOutlet weak var connectButton: UIButton!
@@ -37,33 +49,118 @@ class ViewController: UIViewController {
     @IBOutlet weak var ledSwitch2: UISwitch!
     @IBOutlet weak var ledSwitch3: UISwitch!
     @IBOutlet weak var ledSwitchJack: UISwitch!
+    @IBOutlet weak var ledSwitchMaster: UISwitch!
 
     @IBOutlet weak var ledSlider1: UISlider!
     @IBOutlet weak var ledSlider2: UISlider!
     @IBOutlet weak var ledSlider3: UISlider!
     @IBOutlet weak var ledSliderJack: UISlider!
+    @IBOutlet weak var ledSliderMaster: UISlider!
 
     @IBOutlet weak var batteryMeter1: BatteryView!
     @IBOutlet weak var batteryMeter2: BatteryView!
     @IBOutlet weak var batteryMeter3: BatteryView!
     @IBOutlet weak var batteryMeterJack: BatteryView!
-
-    lazy var hellhounds = getHellhounds()
+    @IBOutlet weak var batteryMeterMaster: BatteryView!
     
-    func getHellhounds() -> [(ledSwitch: UISwitch, ledSlider: UISlider, battery: BatteryView)] {
-        return [(ledSwitch1, ledSlider1, batteryMeter1),
-                (ledSwitch2, ledSlider2, batteryMeter2),
-                (ledSwitch3, ledSlider3, batteryMeter3),
-                (ledSwitchJack, ledSliderJack, batteryMeterJack),
+    @IBOutlet weak var hellhoundView1: UIView!
+    @IBOutlet weak var hellhoundView2: UIView!
+    @IBOutlet weak var hellhoundView3: UIView!
+    @IBOutlet weak var hellhoundViewJack: UIView!
+    @IBOutlet weak var hellhoundViewMaster: UIView!
+
+    // MARK: - Overlay
+    var overlayView: UIView!
+    var overlayLabel: UILabel!
+    var overlaySpinner: UIActivityIndicatorView!
+
+    func setupOverlay() {
+        overlayView = UIView(frame: view.bounds)
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        overlayView.isHidden = true
+
+        overlaySpinner = UIActivityIndicatorView(style: .large)
+        // TODO: Make it white
+        overlaySpinner.color = .white
+        overlaySpinner.center = overlayView.center
+        overlaySpinner.startAnimating()
+
+        overlayLabel = UILabel()
+        overlayLabel.textColor = .white
+        overlayLabel.textAlignment = .center
+        overlayLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        overlayView.addSubview(overlaySpinner)
+        overlayView.addSubview(overlayLabel)
+
+        view.addSubview(overlayView)
+
+        // Center the label below the spinner
+        NSLayoutConstraint.activate([
+            overlayLabel.centerXAnchor.constraint(equalTo: overlayView.centerXAnchor),
+            overlayLabel.topAnchor.constraint(equalTo: overlaySpinner.bottomAnchor, constant: 20)
+        ])
+    }
+
+    func showOverlay(withText text: String) {
+        overlayLabel.text = text
+        overlayView.isHidden = false
+    }
+
+    func hideOverlay() {
+        overlayView.isHidden = true
+    }
+
+    // MARK: -
+
+    var hellhounds: [Hellhound] = []
+    var masterHellhound: Hellhound!
+    
+    func getHellhounds() -> [Hellhound] {
+        return [
+            Hellhound(ledSwitch: ledSwitch1, ledSlider: ledSlider1, battery: batteryMeter1, view: hellhoundView1),
+            Hellhound(ledSwitch: ledSwitch2, ledSlider: ledSlider2, battery: batteryMeter2, view: hellhoundView2),
+            Hellhound(ledSwitch: ledSwitch3, ledSlider: ledSlider3, battery: batteryMeter3, view: hellhoundView3),
+            Hellhound(ledSwitch: ledSwitchJack, ledSlider: ledSliderJack, battery: batteryMeterJack, view: hellhoundViewJack)
         ]
     }
 
+    func getMasterHellhound() -> Hellhound {
+        return Hellhound(ledSwitch: ledSwitchMaster, ledSlider: ledSliderMaster, battery: batteryMeterMaster, view: hellhoundViewMaster)
+    }
+
     func mqttConnect() {
+        connectionState = .connecting
         _ = mqtt!.connect()
     }
 
+    var connectionState: ConnectionState = .disconnected {
+        didSet {
+        print("Connection state changed to: \(connectionState)")
+            updateConnectionUI()
+        }
+    }
+
+    func updateConnectionUI() {
+        switch connectionState {
+        case .connected:
+            connectButton.setTitle("Connected", for: .normal)
+            connectButton.isSelected = true
+            hideOverlay()
+        case .connecting:
+            connectButton.setTitle("Connecting...", for: .normal)
+            connectButton.isSelected = false
+            showOverlay(withText: "Connecting...")
+        case .disconnected:
+            connectButton.setTitle("Connect", for: .normal)
+            connectButton.isSelected = false
+            hideOverlay()
+        }
+    }
+
     @IBAction func connectToServer() {
-        connectSpinner.startAnimating()
+        print("connectToServer pressed")
+        connectionState = .connecting
         mqttConnect()
     }
     
@@ -84,13 +181,12 @@ class ViewController: UIViewController {
     func mqttSetBrightness(value: Int, hellhoundIndex: Int) {
         let topic = fullTopicFor(topic: kTopicSetLevel, hellhoundIndex: hellhoundIndex)
         let stateValue = String(value)
-        TRACE("SET BRIGHTNESS TOPIC: \(topic), SV:\(stateValue)")
+        print("SET BRIGHTNESS TOPIC: \(topic), SV:\(stateValue)")
         mqtt!.publish(topic, withString: stateValue, qos: .qos0)
     }
     
     func mqttGetStatus() {
-        for i in 0..<kNumHellhounds
-        {
+        for i in 0..<hellhounds.count {
             let topic = fullTopicFor(topic: kTopicGetStatus, hellhoundIndex: i)
             // value is ignored. "0" arbitrary
             mqtt!.publish(topic, withString: "0", qos: .qos0)
@@ -98,41 +194,33 @@ class ViewController: UIViewController {
     }
     
     func mqttSubscribeStatus() {
-        for i in 0..<kNumHellhounds
-        {
-//            let subscribeTopic = fullTopicFor(topic: kTopicStatus, hellhoundIndex: i)
-//            mqtt!.subscribe(subscribeTopic, qos: CocoaMQTTQOS.qos1)
+        for i in 0..<hellhounds.count {
             let subscribeJSONTopic = fullTopicFor(topic: kTopicStatusJSON, hellhoundIndex: i)
             mqtt!.subscribe(subscribeJSONTopic, qos: CocoaMQTTQoS.qos1)
         }
     }
     
     func mqttHandleMessage(topic: String, message: String) {
-        connectButton.isSelected = true
-        connectSpinner.stopAnimating()
-
         let topicComponents = topic.components(separatedBy: "/")
-        assert(topicComponents.count == 3);
-        let hellhoundId = topicComponents[1];
+        assert(topicComponents.count == 3)
+        let hellhoundId = topicComponents[1]
         if topicComponents[2] == kTopicStatusJSON {
             do {
                 let filteredMessage = message.filter { !"\\".contains($0) }
-                print (filteredMessage)
                 let data = Data(filteredMessage.utf8)
-                if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [String: Int]
-                {
-                   print(jsonArray) // use the json here
+                if let jsonArray = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Int] {
+//                    print(jsonArray) // use the json here
                     let ledState = (jsonArray["ledState"] == 1)
                     let brightness = jsonArray["ledLvl"]!
                     let battery = jsonArray["battery"]!
                     updateHellhoundView(index: Int(hellhoundId) ?? 0, isOn: ledState, brightness: brightness, batteryLevel: battery)
                 } else {
-                    print("bad json")
+                    print("bad json\n\(filteredMessage)")
                 }
             } catch let error as NSError {
                 print(error)
             }
-            return;
+            return
         }
     }
     
@@ -144,26 +232,42 @@ class ViewController: UIViewController {
         mqttSetBrightness(value: Int(round(sender.value)), hellhoundIndex: sender.tag)
     }
 
+    @IBAction func ledMasterSwitchValueChanged(_ sender: UISwitch) {
+        for i in 0..<hellhounds.count {
+            mqttSetState(value: sender.isOn, hellhoundIndex: i)
+        }
+    }
+    
+    @IBAction func ledMasterBrightnessValueChanged(_ sender: UISlider) {
+        for i in 0..<hellhounds.count {
+            mqttSetBrightness(value: Int(round(sender.value)), hellhoundIndex: i)
+        }
+    }
+
     @IBAction func masterButtonPressed(_ sender: UIButton) {
-        let isOn = sender==masterOnButton
-        for i in 0..<kNumHellhounds {
+        let isOn = sender == masterOnButton
+        for i in 0..<hellhounds.count {
             mqttSetState(value: isOn, hellhoundIndex: i)
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupOverlay()
+        hellhounds = getHellhounds()
+        masterHellhound = getMasterHellhound()
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false
         tabBarController?.delegate = self
         mqttSetting()
         mqttConnect()
         
-        for hound in hellhounds {
+        for hound in hellhounds + [masterHellhound!] {
             hound.ledSlider.maximumValue = 100.0
             hound.ledSlider.isContinuous = false
             hound.battery.direction = .maxXEdge
             hound.battery.level = 4
             hound.battery.lowThreshold = 20
+            hound.view.alpha = 0.3
         }
 
         if let tabBarController = self.tabBarController {
@@ -179,11 +283,41 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.isHidden = false
     }
     
+    // update master hellhound view to highest value of all hellhounds for isOn and brightness
+    func updateMasterHellhoundView() {
+        var isOn = false
+        var brightness: Float = 0
+        var batteryLevel: Int? = nil
+        var isConnected = false
+        for hound in hellhounds where hound.isConnected {
+            if hound.ledSwitch.isOn {
+                isOn = true
+            }
+            if hound.ledSlider.value > brightness {
+                brightness = hound.ledSlider.value
+            }
+            if batteryLevel == nil || hound.battery.level < batteryLevel! {
+                batteryLevel = hound.battery.level
+            }
+            isConnected = true
+        }
+        masterHellhound.ledSwitch.isOn = isOn
+        masterHellhound.ledSlider.value = brightness
+        if let batteryLevel = batteryLevel {
+            masterHellhound.battery.level = batteryLevel
+        }
+        masterHellhound.isConnected = isConnected
+        masterHellhound.view.alpha = isConnected ? 1.0 : 0.3
+    }
+    
     func updateHellhoundView(index: Int, isOn: Bool, brightness: Int, batteryLevel: Int) {
-        TRACE("index: \(index) isOn: \(isOn) brightness: \(brightness) batteryLevel: \(batteryLevel)")
+        print("updateHellhoundView: index: \(index) isOn: \(isOn) brightness: \(brightness) batteryLevel: \(batteryLevel)")
         hellhounds[index].ledSwitch.isOn = isOn
         hellhounds[index].ledSlider.value = Float(brightness)
         hellhounds[index].battery.level = Int(batteryLevel)
+        hellhounds[index].isConnected = true
+        hellhounds[index].view.alpha = 1.0
+        updateMasterHellhoundView()
     }
     
     func mqttSetting() {
@@ -195,7 +329,7 @@ class ViewController: UIViewController {
         mqtt!.keepAlive = 60
         mqtt!.delegate = self
         mqtt!.didReceiveMessage = { mqtt, message, id in
-            print("Message received in topic \(message.topic) with payload \(message.string!)")
+//            print("Message received in topic \(message.topic) with payload \(message.string!)")
         }
         
     }
@@ -211,9 +345,9 @@ class ViewController: UIViewController {
         
         // create key dictionary for reading p12 file
         let key = kSecImportExportPassphrase as String
-        let options : NSDictionary = [key: certPassword]
+        let options: NSDictionary = [key: certPassword]
         
-        var items : CFArray?
+        var items: CFArray?
         let securityError = SecPKCS12Import(p12Data, options, &items)
         
         guard securityError == errSecSuccess else {
@@ -260,10 +394,13 @@ extension ViewController: CocoaMQTTDelegate {
         TRACE("ack: \(ack)")
 
         if ack == .accept {
+            connectionState = .connected
             self.mqttSubscribeStatus()
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                 self.mqttGetStatus()
             }
+        } else {
+            connectionState = .disconnected
         }
     }
     
@@ -280,9 +417,9 @@ extension ViewController: CocoaMQTTDelegate {
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
-        let messageString = message.string ?? kEmptyResponse;
+        let messageString = message.string ?? kEmptyResponse
         TRACE("message: \(messageString), id: \(id)")
-        mqttHandleMessage(topic: message.topic, message: messageString);
+        mqttHandleMessage(topic: message.topic, message: messageString)
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopics success: NSDictionary, failed: [String]) {
@@ -303,6 +440,7 @@ extension ViewController: CocoaMQTTDelegate {
 
     func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
         TRACE("\(err.description)")
+        connectionState = .disconnected
     }
 }
 
